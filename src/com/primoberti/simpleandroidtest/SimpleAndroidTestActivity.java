d1d962ce09b00c127ffeb3d60a312b41e818f7a4
Alberto Salmerón Moreno <alberto.salmeron@gmail.com>
2012-02-06 22:52:45 +0100

    Added about dialog

--- src/com/primoberti/simpleandroidtest/SimpleAndroidTestActivity.java
+++ src/com/primoberti/simpleandroidtest/SimpleAndroidTestActivity.java
@@ -20,13 +20,47 @@
 package com.primoberti.simpleandroidtest;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class SimpleAndroidTestActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		Button button = (Button) findViewById(R.id.aboutButton);
+		button.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				showAboutDialog();
+			}
+		});
+	}
+
+	private void showAboutDialog() {
+		AlertDialog.Builder builder = new AlertDialog.Builder(
+				SimpleAndroidTestActivity.this);
+		builder.setTitle(R.string.about_dialog_title);
+		builder.setMessage(R.string.about_dialog_content);
+		builder.setPositiveButton(android.R.string.ok,
+				new DialogInterface.OnClickListener() {
+
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						dialog.dismiss();
+					}
+				});
+
+		AlertDialog dialog = builder.create();
+		dialog.show();
+	}
+
 }
