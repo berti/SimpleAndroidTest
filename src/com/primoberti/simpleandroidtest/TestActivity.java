eb858b99a875d44cb9f5e6c52e46e8948e3ddf45
Alberto Salmerón Moreno <alberto.salmeron@gmail.com>
2012-02-08 19:35:51 +0100

    Removed unused import

--- src/com/primoberti/simpleandroidtest/TestActivity.java
+++ src/com/primoberti/simpleandroidtest/TestActivity.java
@@ -23,7 +23,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
